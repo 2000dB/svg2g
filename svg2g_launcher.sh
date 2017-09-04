5b518d10ee7aa335b4e34d90622f22a3b890a679
Vincent de Belleval <2000db@gmail.com>
2017-09-04 16:24:43 +0200

    make margin and paper arguments in the script

--- svg2g_launcher.sh
+++ svg2g_launcher.sh
@@ -3,4 +3,8 @@
 SVG2G="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 SVG2G=$SVG2G/src/svg2g.py
 
-$SVG2G --start-delay=100 --stop-delay=100 --xy-feedrate=3000 --y-home=103 --z-home=125 --xoffset=64 --paper-length=100 $1 > $2
+marginX=$3
+paperLength=$4
+echo $4
+# Distance between the blade cutter and the pen is 94mm so a 64mm xoffset = margin of 30mm
+$SVG2G --start-delay=100 --stop-delay=100 --xy-feedrate=3000 --y-home=103 --z-home=125 --xoffset=$3 --paper-length=$4 $1 > $2
